f1111d30adb101e5237de64c4d92fb122f63c53f
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-11 14:44:56 -1000

    add user entity

---
+++ blog/src/main/java/com/hifishing/blog/model/User.java
@@ -0,0 +1,33 @@
+package com.hifishing.blog.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
