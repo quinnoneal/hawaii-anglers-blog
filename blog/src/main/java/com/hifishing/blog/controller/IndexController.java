c66e3a0b46337703d06f4595ee98589424bf0b03
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-11 20:01:49 -1000

    add index controller

--- blog/src/main/java/com/hifishing/blog/controller/IndexController.java
+++ blog/src/main/java/com/hifishing/blog/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String returnIndex() {
         return "index";
     }
