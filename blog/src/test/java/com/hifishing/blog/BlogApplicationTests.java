bec3cc8c327e4c160c57291e0fa8e0172856647d
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-11 14:12:21 -1000

    add empty spring boot project

---
+++ blog/src/test/java/com/hifishing/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hifishing.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
