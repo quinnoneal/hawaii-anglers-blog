b2c2ed20ae5bd1bc4ae04d79e4e1e8fd2808dc02
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-18 23:50:30 -1000

    inject postservice dependency to extract list of posts to add to thymeleaf model

--- blog/src/main/java/com/hifishing/blog/controller/IndexController.java
+++ blog/src/main/java/com/hifishing/blog/controller/IndexController.java
@@ -1,13 +1,28 @@
 package com.hifishing.blog.controller;
 
+import com.hifishing.blog.model.Post;
+import com.hifishing.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private final PostService postService;
+
+    @Autowired
+    public IndexController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/")
-    public String returnIndex() {
+    public String returnIndex(Model model) {
+        List<Post> posts = postService.findAll();
+        model.addAttribute("posts", posts);
         return "index";
     }
 }
