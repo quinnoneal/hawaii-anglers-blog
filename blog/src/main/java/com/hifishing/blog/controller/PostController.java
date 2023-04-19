3854c1ef6e18ffacc019a3fb844efe4efa077aea
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-18 19:21:56 -1000

    add posts home mapping

--- blog/src/main/java/com/hifishing/blog/controller/PostController.java
+++ blog/src/main/java/com/hifishing/blog/controller/PostController.java
@@ -1,4 +1,13 @@
 package com.hifishing.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    public String returnPostsPage() {
+        return "posts.html";
+    }
 }
