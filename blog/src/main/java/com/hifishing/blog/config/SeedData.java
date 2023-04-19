99416e8d335e7ad8df00e04f9854b995392fcc01
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-18 23:51:36 -1000

    add more dummy data for testing

--- blog/src/main/java/com/hifishing/blog/config/SeedData.java
+++ blog/src/main/java/com/hifishing/blog/config/SeedData.java
@@ -35,10 +35,36 @@ public class SeedData implements CommandLineRunner {
 
         Post post = new Post();
         post.setUser(user);
-        post.setTitle("My First Blog Post");
+        post.setTitle("My First Blog Post!");
+        post.setDescription("This is my very first post on my new blog. This blog is for posts that have to do with Hawaii fishing in any way shape or form. Please join me on this adventure and create an account to post or comment on others' posts! Thanks for stopping by.");
         post.setBody("Hello world! This is my first fishing blog post! Shaka braddahz!");
         post.setDatePosted(LocalDateTime.now());
 
+        Post post2 = new Post();
+        post2.setUser(user);
+        post2.setTitle("What is Lorem Ipsum?");
+        post2.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.");
+        post2.setBody("Hello world! This is my first fishing blog post! Shaka braddahz!");
+        post2.setDatePosted(LocalDateTime.now());
+
+        Post post3 = new Post();
+        post3.setUser(user);
+        post3.setTitle("What is Lorem Ipsum?");
+        post3.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.");
+        post3.setBody("Hello world! This is my first fishing blog post! Shaka braddahz!");
+        post3.setDatePosted(LocalDateTime.now());
+
+        Post post4 = new Post();
+        post4.setUser(user);
+        post4.setTitle("What is Lorem Ipsum?");
+        post4.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.");
+        post4.setBody("Hello world! This is my first fishing blog post! Shaka braddahz!");
+        post4.setDatePosted(LocalDateTime.now());
+
         postService.save(post);
+        postService.save(post2);
+        postService.save(post3);
+        postService.save(post4);
+
     }
 }
