671837973147bb4b264d4d06690fea15bbfb1085
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-04-28 19:48:52 -1000

    add get mapping for individual post

--- blog/src/main/java/com/hifishing/blog/controller/PostController.java
+++ blog/src/main/java/com/hifishing/blog/controller/PostController.java
@@ -1,13 +1,32 @@
 package com.hifishing.blog.controller;
 
+import com.hifishing.blog.model.Post;
+import com.hifishing.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/posts")
     public String returnPostsPage() {
-        return "posts.html";
+        return "posts";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long postId, Model model) {
+        Post post = postService.findById(postId);
+        model.addAttribute("post", post);
+        return "post";
     }
 }
