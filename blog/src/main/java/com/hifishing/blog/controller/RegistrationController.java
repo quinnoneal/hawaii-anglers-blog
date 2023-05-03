b685d7f17eec060e04043b64420f060bfcfbc2fc
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-02 14:31:47 -1000

    implement simple user registration

--- blog/src/main/java/com/hifishing/blog/controller/RegistrationController.java
+++ blog/src/main/java/com/hifishing/blog/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.hifishing.blog.model.User;
 import com.hifishing.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,15 @@ public class RegistrationController {
     }
 
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(Model model) {
+        User newUser = new User();
+        model.addAttribute("user", newUser);
         return "register";
     }
 
     @PostMapping("/register")
     public String registerNewUser(@ModelAttribute User user) {
         userService.save(user);
+        return "redirect:/";
     }
 }
