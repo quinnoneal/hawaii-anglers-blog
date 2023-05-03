fe76134ce14c665d36b79704ab43cdc5b486234f
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-02 15:13:11 -1000

    add post creation functionality

--- blog/src/main/java/com/hifishing/blog/controller/PostController.java
+++ blog/src/main/java/com/hifishing/blog/controller/PostController.java
@@ -1,21 +1,29 @@
 package com.hifishing.blog.controller;
 
 import com.hifishing.blog.model.Post;
+import com.hifishing.blog.model.User;
 import com.hifishing.blog.service.PostService;
+import com.hifishing.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class PostController {
 
     private final PostService postService;
+    private final UserService userService;
 
     @Autowired
-    public PostController(PostService postService) {
+    public PostController(PostService postService, UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping("/posts")
@@ -28,5 +36,24 @@ public class PostController {
         Post post = postService.findById(id);
         model.addAttribute("post", post);
         return "post";
+    }
+
+    @GetMapping("/posts/new")
+    public String createNewPost(Model model) {
+        Optional<User> optionalUser = userService.findByEmail("quinnoneal@hotmail.com");
+        if(optionalUser.isPresent()) {
+            Post post = new Post();
+            post.setUser(optionalUser.get());
+            model.addAttribute("post", post);
+            return "post_new";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/new")
+    public String saveNewPost(@ModelAttribute Post post) {
+        postService.save(post);
+        return "redirect:/posts/" + post.getId();
     }
 }
