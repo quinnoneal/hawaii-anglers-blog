23b73dc9a122e8dc6654624f490a128d11f3b093
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-05 23:04:10 -1000

    initial security filter chain

--- blog/src/main/java/com/hifishing/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/hifishing/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hifishing.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,20 +10,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // list of endpoints that non-logged in users can access
+    private static final String[] WHITELIST = {
+            "/",
+            "/shop",
+            "/posts",
+            "/sign-in",
+            "/register",
+            "/h2-console/**",
+            "/resources/**"
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().permitAll());
+                        .requestMatchers(WHITELIST).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
+                        .anyRequest().authenticated());
         // use basic authentication
         http.httpBasic();
 
         // disable csrf
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+        //http.csrf().disable();
+        //http.headers().frameOptions().disable();
 
         return http.build();
     }
