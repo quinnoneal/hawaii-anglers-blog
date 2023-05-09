828ef0fefa251dc894da015c405c108bb68a1bc9
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2023-05-08 21:18:30 -1000

    create custom user details service

---
+++ blog/src/main/java/com/hifishing/blog/service/MyUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.hifishing.blog.service;
+
+import com.hifishing.blog.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component("userDetailsService")
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public MyUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userService.findByUsername(username);
+
+        if(optionalUser.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        List<GrantedAuthority> grantedAuthorities = user
+                .getAuthorities()
+                .stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+
+}
